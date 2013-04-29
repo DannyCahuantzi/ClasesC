56552c5c303cf65d32736d83ec4d902deb6c8b94
Arturo-MR <arturo10.mrmail.com>
2013-04-29 00:16:40 -0500

    Se termino el archivo compara

--- Nivel3/compara.c
+++ Nivel3/compara.c
@@ -2,14 +2,46 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
-//Programa:
+//Programa: Validar si el usuario lee el tipo de dato correctamente.
 
 int main(int argc, char *argv[])
 {
-	char num[31];
-	int z;
-	printf("Dame un numero\n");
-	gets(num);
+	long int N;
+	int i,x,y;
+	char Numero[6];
+	int Bandera;
+	char Num;
+//Instrucciones Ejecutables
+	//printf("Por favor Ingrese un numero entero hasta 5 cifras\n");
+	do
+	{
+		do
+		{
+			Bandera = 1;
+			printf("Ingresa un numero\n", );
+			gets(Numero);
+			for (i = 0; i < strlen(Numero)-1; i++)
+			{
+				Num=Numero[i];
+				if (Num<'0' || Num>'9')if (Num!='-') Bandera=0;
+				if (Num == '-' && i>0) Bandera=0;
+				
+			}
+		} while (Bandera=1);
+
+		N = atol(Numero);
+		if (N < -32768 || N > 32767)
+		{
+			Bandera = 0;
+		}else{
+			Bandera = 1;
+		}
+
+	} while (Bandera!=1);
+	printf("El numero es: %d\n\n",N );
+
+//FIN	
+//HOLA?
 	
 	
 
@@ -18,3 +50,4 @@ int main(int argc, char *argv[])
   return 0;
 }
 //AUTOR: LUIS ARTURO MENDOZA
+
