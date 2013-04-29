d7e3115afe8787128a031dfd54b3310f93607238
Arturo-MR <arturo10.mrmail.com>
2013-04-28 23:21:15 -0500

    Iniciando la biblioteca

---
+++ Nivel3/compara.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+//Programa:
+
+int main(int argc, char *argv[])
+{
+	char num[31];
+	int z;
+	printf("Dame un numero\n");
+	gets(num);
+	
+	
+
+//Sistema de pausa
+	system("PAUSE");	
+  return 0;
+}
+//AUTOR: LUIS ARTURO MENDOZA
